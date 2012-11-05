64c32897da2f3731d7e376f0c1c31a6259c17e69
tunnr <tunnerkrisztian@freemail.hu>
2012-11-05 18:04:00 +0100

    fix script names

---
+++ Updates/1105_world.sql
@@ -0,0 +1,8 @@
+UPDATE creature_template SET `ScriptName` = 'npc_lunaclaw_spirit' WHERE entry = 12144;
+UPDATE creature_template SET `ScriptName` = 'mob_mojo' WHERE entry = 8296;
+UPDATE creature_template SET `ScriptName` = 'mob_fiendish_portal' WHERE entry = 17265;
+UPDATE creature_template SET `ScriptName` = 'npc_kalecgos' WHERE entry = 24848;
+UPDATE creature_template SET `ScriptName` = 'mob_prowler' WHERE entry = 15101;
+UPDATE gameobject_template SET `ScriptName` = 'go_cat_figurine' WHERE entry = 13359;
+UPDATE creature_template SET `ScriptName` = 'at_ravenholdt' WHERE entry = 13936;
+/*UPDATE creature_template SET `ScriptName` = 'at_coilfang_waterfall' WHERE entry = ?????;*/
